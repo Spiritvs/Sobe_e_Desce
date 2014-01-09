e34f676e28a9c027903c62e21e3fc45780717208
Spiritvs <m.ricardo.sa@gmail.com>
2014-01-09 01:13:10 +0000

    V2

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var nJogadores = 0;
+//Guarda o numero de Jogadores para construir as divs de acordo
 var pontosI = 20;
+//Guarda os pontos com que se começa
 var pontos = [];
-var soma = [5];
+//Guarda os Pontos de Cada Um
+var soma = 5;
+//Auxiliar nas Contas
 var nomes = [];
+//Guarda os Nomes dos Jogadores
 var divPontos = [];
+//Para criar a div nova com a actulização dos pontos
 var dinheiro = [];
+//Guarda os ganhos de cada um
 var dim;
+//Dimensão das Divs
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
@@ -16,81 +24,99 @@ function onDeviceReady() {
 	document.getElementById('topContainer').setAttribute("style", "height:" + dim + "px");
 }
 
-function teste(){
+function teste() {
 	var div = document.createElement('div');
-	div.setAttribute("id","teste");
+	div.setAttribute("id", "teste");
 	document.getElementById("body").appendChild(div);
 }
+
 function inserirDados() {
-	if(confirm("Deseja Começar um jogo Novo? Todos os dados anteriores serão apagados.")){
-	var container = document.getElementById('midContainer');
-	var container2 = document.getElementById('bottomContainer');
-			while (container.firstChild) {
-			container.removeChild(container.firstChild);
+	if (confirm("Deseja Começar um jogo Novo? Todos os dados anteriores serão apagados.")) {
+		var midContainer = document.getElementById('midContainer');
+		var bottomContainer = document.getElementById('bottomContainer');
+		while (midContainer.firstChild) {
+			midContainer.removeChild(midContainer.firstChild);
 		}
-			while (container2.firstChild) {
-		container2.removeChild(container2.firstChild);
-	}
-	do {
-		nJogadores = prompt("Quantos Jogadores? (4 ou 5)");
-	} while(nJogadores!=4 && nJogadores!=5);
+		while (bottomContainer.firstChild) {
+			bottomContainer.removeChild(bottomContainer.firstChild);
+		}
+		do {
+			nJogadores = prompt("Quantos Jogadores? (4 ou 5)");
+		} while(nJogadores!=4 && nJogadores!=5);
 
-	do {
-		pontosI = prompt("Quantos Pontos?");
-	} while(isNaN(pontosI) || pontosI<=0);
-
-	for (var i = 1; i <= nJogadores; i++) {
-		do{
-		nomes[i] = prompt("Nome do Jogador " + i + "?");
-		}while(nomes[i] == "" || nomes[i] == null);
-		var div = document.createElement('div');
-		div.setAttribute("class", "jogadores");
-		div.setAttribute("id", "div" + i);
+		do {
+			pontosI = prompt("Quantos Pontos?");
+		} while(isNaN(pontosI) || pontosI<=0);
 		if (nJogadores == 4) {
-			div.setAttribute("style", "width:23%");
-		};
-		container.appendChild(div);
-		document.getElementById('div' + i).innerHTML = nomes[i];
-		divPontos[i] = document.createElement('div');
-		divPontos[i].innerHTML = pontosI;
-		divPontos[i].setAttribute("style", "width:100%");
-		divPontos[i].setAttribute("style", "height:auto");
-		document.getElementById('div' + i).appendChild(divPontos[i]);
-		pontos[i] = pontosI;
-	}
+
+		}
+		for (var i = 1; i <= nJogadores; i++) {
+			do {
+				nomes[i] = prompt("Nome do Jogador " + i + "?");
+			} while(nomes[i] == "" || nomes[i] == null);
+			var div = document.createElement('div');
+			div.setAttribute("class", "jogadores");
+			div.setAttribute("id", "div" + i);
+			if (nJogadores == 4) {
+				div.setAttribute("style", "width:23%");
+			};
+			midContainer.appendChild(div);
+
+			var nome = document.createElement("div");
+			nome.setAttribute("class", "nome");
+			nome.innerHTML = nomes[i];
+			div.appendChild(nome);
+
+			var din = document.createElement("div");
+			din.setAttribute("class", "dinheiro");
+			din.setAttribute("id","dinheiro"+i);
+			dinheiro[i] = 0;
+			din.innerHTML = "$:" + dinheiro[i];
+			div.appendChild(din);
+
+			divPontos[i] = document.createElement("div");
+			divPontos[i].innerHTML = pontosI;
+			divPontos[i].setAttribute("style", "width:100%; height:auto;");
+			div.appendChild(divPontos[i]);
+			pontos[i] = pontosI;
+		}
 	}
 }
 
 function fimJogada() {
-
 	for (var i = 1; i <= nJogadores; i++) {
-		soma[i] = prompt("Quantas vazas fez o " + nomes[i] + "?");
-		if(soma[i]){
-		if (isNaN(soma[i])) {
-			divPontos[i] = document.createElement('div');
-			divPontos[i].innerHTML = "--";
-			divPontos[i].setAttribute("style", "width:100%");
-			divPontos[i].setAttribute("style", "height:auto");
-			document.getElementById('div' + i).appendChild(divPontos[i]);
+		if(pontos[i]<=0){return;}
+	}
+	for (var i = 1; i <= nJogadores; i++) {
+		if(pontos[i]<=0){break;}
+		soma = prompt("Quantas vazas fez o " + nomes[i] + "?");
+		if (soma) {
+			if (isNaN(soma)) {
+				pontos[i] = parseInt(pontos[i], 10);
+				divPontos[i] = document.createElement('div');
+				divPontos[i].innerHTML = "--";
+				divPontos[i].setAttribute("style", "width:100%");
+				divPontos[i].setAttribute("style", "height:auto");
+				document.getElementById("div" + i).appendChild(divPontos[i]);
+			} else {
+				if (soma == 0) {
+					soma = -5;
+				};
+				pontos[i] = parseInt(pontos[i], 10) - parseInt(soma, 10);
+				divPontos[i] = document.createElement('div');
+				divPontos[i].innerHTML = pontos[i];
+				divPontos[i].setAttribute("style", "width:100%");
+				divPontos[i].setAttribute("style", "height:auto");
+				document.getElementById("div" + i).appendChild(divPontos[i]);
+			}
 		} else {
-			if (soma[i] == 0) {
-				soma[i] = -5
-			};
-			pontos[i] = parseInt(pontos[i], 10) - parseInt(soma[i], 10);
-			divPontos[i] = document.createElement('div');
-			divPontos[i].innerHTML = pontos[i];
-			divPontos[i].setAttribute("style", "width:100%");
-			divPontos[i].setAttribute("style", "height:auto");
-			document.getElementById('div' + i).appendChild(divPontos[i]);
-		}
-		}
-		else{
+			pontos[i] = parseInt(pontos[i], 10);
 			divPontos[i] = document.createElement('div');
 			divPontos[i].innerHTML = "--";
 			divPontos[i].setAttribute("style", "width:100%");
 			divPontos[i].setAttribute("style", "height:auto");
-			document.getElementById('div' + i).appendChild(divPontos[i]);
-			
+			document.getElementById("div" + i).appendChild(divPontos[i]);
+
 		}
 	}
 	for (var i = 1; i <= nJogadores; i++) {
@@ -106,34 +132,65 @@ function fimJogada() {
 			div.appendChild(img);
 			div.appendChild(text);
 			document.getElementById('bottomContainer').appendChild(div);
-			break;
+			calculos();
+			return;
 		};
 	}
 }
 
-function novoJogo() {
-	for (var i = 1; i <= nJogadores; i++) {
-		var container = document.getElementById('div' + i);
-		var container2 = document.getElementById('bottomContainer');
-		while (container.firstChild) {
-			container.removeChild(container.firstChild);
+function calculos(){
+	for (var i = 1; i <= nJogadores; i++){
+		if(pontos[i]<=0){
+			pontos[i]=0;
+			var aux = i;
+			for (var j = 1; j <= nJogadores; j++){
+				dinheiro[aux]+=pontos[j];
+				}		
+			}
+		else{
+			dinheiro[i]-=pontos[i];
 		}
+	document.getElementById('dinheiro'+i).innerHTML = "$:" + dinheiro[i];
+	}
+}
+
+function novoJogo() {
+	var midContainer = document.getElementById('midContainer');
+	while (midContainer.firstChild) {
+		midContainer.removeChild(midContainer.firstChild);
+	}
+	for (var i = 1; i <= nJogadores; i++) {
+
+		var bottomContainer = document.getElementById('bottomContainer');
+
 		var div = document.createElement('div');
 		div.setAttribute("class", "jogadores");
 		div.setAttribute("id", "div" + i);
 		if (nJogadores == 4) {
-			div.setAttribute("style", "width:25%");
+			div.setAttribute("style", "width:23%");
 		};
-		document.getElementById('midContainer').appendChild(div);
-		document.getElementById('div' + i).innerHTML = nomes[i];
-		divPontos[i] = document.createElement('div');
+		midContainer.appendChild(div);
+
+		var nome = document.createElement("div");
+		nome.setAttribute("class", "nome");
+		nome.innerHTML = nomes[i];
+		div.appendChild(nome);
+
+		var din = document.createElement("div");
+		din.setAttribute("class", "dinheiro");
+		din.setAttribute("id","dinheiro"+i);
+		din.innerHTML = "$:" + dinheiro[i];
+		div.appendChild(din);
+
+		divPontos[i] = document.createElement("div");
 		divPontos[i].innerHTML = pontosI;
-		divPontos[i].setAttribute("style", "width:100%");
-		divPontos[i].setAttribute("style", "height:auto");
-		document.getElementById('div' + i).appendChild(divPontos[i]);
+		divPontos[i].setAttribute("style", "width:100%; height:auto;");
+		div.appendChild(divPontos[i]);
 		pontos[i] = pontosI;
+
 	}
-	while (container2.firstChild) {
-		container2.removeChild(container2.firstChild);
+	while (bottomContainer.firstChild) {
+		bottomContainer.removeChild(bottomContainer.firstChild);
 	}
 }
+
